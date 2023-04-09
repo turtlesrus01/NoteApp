16a9fa864f47ee21577a32f69f6441b4ff0c72b9
Green Camper <stefan.palacios@gmail.com>
2023-04-08 23:07:12 -0500

    Formatted index.js and added server error handler function

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 //init modules (express)
-const express = require('express');
+const express = require("express");
 
 //import module routes for notes
-const notesRouter = require('./notes');
+const notesRouter = require("./notes");
 const app = express();
 
 //middleware in order to tie router to notes pathway
-app.use('/notes', notesRouter)
+app.use("/notes", notesRouter);
+
+//middleware error handler for server errors
+app.use((err, req, res, next) => {
+  res.status(500).send("Internal Server Error");
+});
 
 module.exports = app;
