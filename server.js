11609a6120999b1d1c4694aa26d1dcf1b078065d
Green Camper <stefan.palacios@gmail.com>
2023-04-08 20:09:04 -0500

    Started pseudocoding on server, and added route files

---
+++ server.js
@@ -0,0 +1,6 @@
+//init modules (fs, uuid, express, path)
+//init middleware (api, urlencoded)
+//port handler 
+//get route for index.html
+//get route for notes.html
+//app listener once server has started
