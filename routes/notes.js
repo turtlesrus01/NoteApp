70a7e759ac6ab5056d0c421d050655ee2e8540ba
Green Camper <stefan.palacios@gmail.com>
2023-04-10 11:18:51 -0500

    Added imports to notes.js, now working on GET and POST routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,13 @@
-//import modules (notes, fsUtil, uuid)
+//import modules (router, fsUtil, uuid)
+const notes =  require('express').Router();
+//const { readAndAppend } = require('../helpers/fsUtils')
+const { v4: uuidv4 } = require('uuid');
+
 //GET for retrieving all notes
+
+
 //POST for submitting new notes
   //destructured items from request
   //if statement to check for all conditions met
   //
+//export statement for notes module
