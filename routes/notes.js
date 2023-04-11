cfff3b92b65aa1bf9537769ea8fd262408b40c22
Green Camper <stefan.palacios@gmail.com>
2023-04-11 00:38:52 -0500

    Format: formatted notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,21 @@
 //import modules (router, fsUtil, uuid)
-const notes =  require('express').Router();
-//const { readAndAppend } = require('../helpers/fsUtils')
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend } = require('../helpers/fsUtils');
+const notes = require("express").Router();
+const { readAndAppend } = require('../helpers/fsUtils')
+const { v4: uuidv4 } = require("uuid");
 
 //GET for retrieving all notes
-notes.get('/', (req , res) => {
-  readFromFile('./db/feedback.json')
-  .then((data) => {
-    res.json(JSON.parse(data));
-  })
-  .catch((err) => {
-    res.status(500).send('Server error');
-  })
+notes.get("/", (req, res) => {
+  readFromFile("./db/feedback.json")
+    .then((data) => {
+      res.json(JSON.parse(data));
+    })
+    .catch((err) => {
+      res.status(500).send("Server error");
+    });
 });
 
 //POST for submitting new notes
-notes.post('/', (req,res) => {
+notes.post("/", (req, res) => {
   //destructured items from request
   const { title, text } = req.body;
   //if statement to check for all conditions met
@@ -27,18 +26,18 @@ notes.post('/', (req,res) => {
       text,
     };
 
-    readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, "./db/db.json");
 
     const response = {
-      status: 'success',
-      body: newNote
+      status: "success",
+      body: newNote,
     };
 
     res.json(response);
   } else {
-    res.status(400).send('Error in posting a new note.')
+    res.status(400).send("Error in posting a new note.");
   }
 });
-  
+
 //export statement for notes module
 module.exports = notes;
