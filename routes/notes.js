153736d8d9eeb2790399121818f8e1a8b32dc9f5
Green Camper <stefan.palacios@gmail.com>
2023-04-14 01:46:51 -0500

    feature: added DELETE route and tested

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 //import modules (router, fsUtil, uuid)
 const notes = require("express").Router();
+const fs = require("fs");
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils')
 const { v4: uuidv4 } = require("uuid");
 
@@ -41,13 +42,37 @@ notes.post("/", (req, res) => {
   }
 });
 
-//DELETE for deleting notes
-notes.delete('/', (req,res) => {
-  console.log(req.body);
-  //readFromFile from db.JSON
-  //search through notes to find ID
-  
-})
+//DELETE route for deleting notes
+notes.delete('/:id', (req, res) => {
+  console.log('Delete request received');
+  //reads content of the json with callback function
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //error handler
+    if (err) return res.status(422).send(err);
+    //try statement to handle more complex code
+    try {
+      //this gives ID the parameter data 
+      const id = req.params.id;
+      //this parses the data from the readFile call
+      const jsonData = JSON.parse(data);
+      //this function will filter out the note of that specific id
+      const eraseNote = jsonData.filter(note => note.id !== id);
+      //creates new json without deleted note 
+      fs.writeFile('./db/db.json', JSON.stringify(eraseNote, null, 2), err => {
+        //error handler callback function
+        if (err) return res.status(422).send(err);
+
+        console.log('Note deleted successfully');
+        res.status(200).send('Note deleted successfully');
+      });
+    //catch error handler
+    } catch (err) {
+      console.error(err);
+      res.status(422).send(err);
+    }
+  });
+});
+
 
 
 //export statement for notes module
